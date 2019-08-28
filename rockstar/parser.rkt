b60635f2eb2dd6e958b907016dfd9f52dcef3fd6
whichxjy <whichxjy@gmail.com>
2019-08-29 03:18:38 +0800

    Add letter

--- rockstar/parser.rkt
+++ rockstar/parser.rkt
@@ -5,3 +5,16 @@ r-line : r-statement
 r-statement : r-assignment
 r-assignment : r-put
 r-put : "Put" r-expr "into" r-var
+letter : upper-case-letter | lower-case-letter
+upper-case-letter : "A" | "B" | "C" | "D" | "E"
+                  | "F" | "G" | "H" | "I" | "J"
+                  | "K" | "L" | "M" | "N" | "O"
+                  | "P" | "Q" | "R" | "S" | "T"
+                  | "U" | "V" | "W" | "X" | "Y"
+                  | "Z"
+lower-case-letter : "a" | "b" | "c" | "d" | "e"
+                  | "f" | "g" | "h" | "i" | "j"
+                  | "k" | "l" | "m" | "n" | "o"
+                  | "p" | "q" | "r" | "s" | "t"
+                  | "u" | "v" | "w" | "x" | "y"
+                  | "z"
