83538ae1ea662d9ccf956377a18c4f045f69596f
whichxjy <whichxjy@gmail.com>
2019-08-31 21:46:09 +0800

    Update lexer

--- rockstar/lexer.rkt
+++ rockstar/lexer.rkt
@@ -115,57 +115,44 @@
     ;; Comment
     [(eqv? (peek-char ip) #\()
      (let ([lex (lexer-srcloc
-                 [(from/to "(" ")")
+                 [(from/to #\( #\))
                   (token 'COMMENT #:skip? #t)])])
        (lex ip))]
     ;; String
     [(eqv? (peek-char ip) #\")
      (token 'STRING (read ip))]
-    ;; Single quote
-    [(eqv? (peek-char ip) #\')
-     (cond
-       [(or (string=? (peek-string 3 0 ip) "'s ")
-            (string=? (peek-string 3 0 ip) "'s\t"))
-        ;; Single quote s
-        (token 'SINGLE-QUOTE-S (read-string 3 ip))]
-       [else
-        ;; Ignored single quote
-        (token 'IGNORED-SINGLE-QUOTE (read-char ip) #:skip? #t)])]
     [else
-     (define lex
-       (lexer-srcloc
-        ;; EOF
-        [(eof) eof]
-        ;; Newline
-        ["\n" (token 'NEWLINE lexeme)]
-        ;; Reserved terms
-        [reserved-terms (token lexeme lexeme)]
-        ;; Whitespace
-        [(:+ whitespace) (token 'WHITESPACE lexeme #:skip? #t)]
-        ;; Simple variable
-        [simple-var
-         (token 'SIMPLE-VAR
-                (string-downcase lexeme))]
-        ;; Common variable
-        [common-var
-         (token 'COMMON-VAR
-                (string-join
-                 (map string-downcase
-                      (string-split lexeme)) "-"))]
-        ;; Proper variable
-        [proper-var
-         (token 'PROPER-VAR
-                (string-join
-                 (map string-titlecase
-                      (string-split lexeme)) "-"))]
-        ;; Pronoun
-        [pronoun
-         (token 'PRONOUN
-                (string-downcase lexeme))]
-        ;; Number
-        [(:or (:+ numeric)
-              (:seq (:+ numeric) "." (:+ numeric)))
-         (token 'NUMBER (string->number lexeme))]))
-     (lex ip)]))
+     (let ([lex (lexer-srcloc
+                 ;; Newline
+                 ["\n" (token 'NEWLINE lexeme)]
+                 ;; Reserved terms
+                 [reserved-terms (token lexeme lexeme)]
+                 ;; Whitespace
+                 [(:+ whitespace) (token 'WHITESPACE lexeme #:skip? #t)]
+                 ;; Simple variable
+                 [simple-var
+                  (token 'SIMPLE-VAR
+                         (string-downcase lexeme))]
+                 ;; Common variable
+                 [common-var
+                  (token 'COMMON-VAR
+                         (string-join
+                          (map string-downcase
+                               (string-split lexeme)) "-"))]
+                 ;; Proper variable
+                 [proper-var
+                  (token 'PROPER-VAR
+                         (string-join
+                          (map string-titlecase
+                               (string-split lexeme)) "-"))]
+                 ;; Pronoun
+                 [pronoun
+                  (token 'PRONOUN
+                         (string-downcase lexeme))]
+                 ;; Number
+                 [(:or (:+ numeric)
+                       (:seq (:+ numeric) "." (:+ numeric)))
+                  (token 'NUMBER (string->number lexeme))])])
+       (lex ip))]))
 
 (provide rockstar-lexer)
