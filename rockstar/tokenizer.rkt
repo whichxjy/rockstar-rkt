e61af8ec5643f761d9e33c7ec94acf105e9a250b
whichxjy <whichxjy@gmail.com>
2019-08-31 21:45:07 +0800

    Preprocess the input-port

--- rockstar/tokenizer.rkt
+++ rockstar/tokenizer.rkt
@@ -2,16 +2,13 @@
 
 (require "lexer.rkt" brag/support)
 
-;; Reserved terms
-(define-lex-abbrev reserved-terms
-  (:or "You" "Me"))
-
 ;; Make tokenizer
 (define (make-tokenizer ip [path #f])
+  (define prep-ip (preprocess ip))
   (port-count-lines! ip)
   (lexer-file-path path)
   (define (next-token)
-    (rockstar-lexer ip))
+    (rockstar-lexer prep-ip))
   next-token)
 
 (provide make-tokenizer)
