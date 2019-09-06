ddc6a3d94fb2745c135967f9e3cde9b7ac4f6851
whichxjy <whichxjy@gmail.com>
2019-09-07 01:20:45 +0800

    Init all variables

--- rockstar/expander.rkt
+++ rockstar/expander.rkt
@@ -2,39 +2,54 @@
 
 (define-macro (r-module-begin (r-program STATEMENT ...))
   (with-pattern
-      ([(NOT-FUNC-DEF-STATEMENT ...)
-        (find-not-func-def-statements #'(STATEMENT ...))]
-       [(GLOBAL-VAR-ID ...)
-        (find-global-var-ids #'(NOT-FUNC-DEF-STATEMENT ...))]
-       [(REAL-STATEMENT ...)
-        (insert-init-statements #:stmt-stxs #'(STATEMENT ...)
-                                #:global-var-ids #'(GLOBAL-VAR-ID ...))])
+      ([(r-program REAL-STATEMENT ...)
+        (init-all-variables #:target-stx #'(r-program STATEMENT ...)
+                            #:global-var-ids '())])
     #'(#%module-begin
-       (r-init-var GLOBAL-VAR-ID) ...
        REAL-STATEMENT ...
        (void))))
 
+;; Phase 1
 (begin-for-syntax
   (require racket/list
            syntax/stx)
   ;; initialize all variables
-  (define (init-all-variables target-stx global-vars)
+  (define (init-all-variables target-stx global-var-ids)
     ;; distinguish r-func-def statements from other types of statements
-    (define-values not-func-def-stxs func-def-stxs
-      (for/lists (not-func-def-stxs func-def-stxs)
-                 ([stx (in-list (stx->list target-stx))]
-                  #:when (syntax-property stx 'r-statement))
-        (cond
-          [(eq? (syntax->datum (stx-car stx)) 'r-func-def)
-           ;; stx is function definition statement
-           (values empty stx)]
-          [else
-           ;; stx isn't function definition statement
-           (values stx empty)])))
+    (define-values (func-def-stxs not-func-def-stxs)
+      (partition (lambda (stx)
+                   (syntax-property stx 'r-func-def))
+                 (stx->list target-stx)))
     ;; find all ids of non-global variables from not-func-def-stxs
-    (define non-global-vars
+    (define non-global-var-ids
       (remove-duplicates
-       (for/list ([stx (in-list (stx-flatten not-func-def-stxs))]
-                  #:when (syntax-property stx 'r-var))
-         stx)
+       (filter (lambda (stx)
+                 (syntax-property stx 'r-var))
+               (stx-flatten not-func-def-stxs))
        #:key syntax->datum))
+    ;; create init statements
+    (define init-stmts
+      (map (lambda (var-stx)
+             (datum->syntax #f
+                            (format-datum '(r-init-var ~a)
+                                          (syntax->datum var-stx))))
+           non-global-var-ids))
+    ;; find the index of first statement in target-stx
+    (define first-stmt-index
+      (index-where (stx->list target-stx)
+                   (lambda (stx)
+                     (syntax-property stx 'r-statement))))
+    ;; split target-stx into two parts
+    (define-values (left-half right-half)
+      (split-at (stx->list target-stx) first-stmt-index))
+    ;; insert init statements to target-stx
+    (define target-stx-with-init-stmts
+      (append left-half init-stmts right-half))
+    ;; process r-func-def in target-stx and return result
+    (for/list ([stx (stx->list target-stx)])
+      (cond
+        [(syntax-property stx 'r-func-def)
+         (init-all-variables stx
+                             (append global-var-ids
+                                         non-global-var-ids))]
+        [else stx]))))
