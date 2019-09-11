7f3c46989f350fa41e198fa5b902e18ea47d77e1
whichxjy <whichxjy@gmail.com>
2019-09-12 05:00:45 +0800

    Create info.rkt

---
+++ rockstar/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "rockstar")
+(define version "1.0")
+(define deps '("base"
+               "beautiful-racket-lib"
+               "brag-lib"
+               "rackunit-lib"))
